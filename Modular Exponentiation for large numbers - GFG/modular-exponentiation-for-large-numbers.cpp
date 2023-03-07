5712c94062dec6659cf1df9a14faa51da02f7c0d
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2023-03-07 15:34:11 +0530

    Added solution - LeetHub

---
+++ Modular Exponentiation for large numbers - GFG/modular-exponentiation-for-large-numbers.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int PowMod(long long int a,long long int b,long long int M)
+		{
+		    // Code here
+		    long long ans = 1;
+		    
+		    while(b){
+		        if(b&1)
+		            ans = (ans%M) * (a%M);
+		        a = (a%M) * (a%M);
+		        b = b >> 1;
+		    }
+		    return ans%M;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int x, n, m;
+    	cin >> x >> n >> m;
+    	Solution ob;
+    	long long int ans = ob.PowMod(x, n, m);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
